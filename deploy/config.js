e7de849933c4ab9cff336507c9bc72bf27d0a2a1
Twiyin0 <87588281+Twiyin0@users.noreply.github.com>
2022-04-14 20:26:26 +0800

    Update config.js

--- deploy/config.js
+++ deploy/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-	path: "/www/wwwroot/blog.yin0mc.ltd/public.zip", //服务器上这个文件要放在哪里
-	host: "blog.yin0mc.ltd", // 服务器的host地址 47.100.208.26
+	path: "/www/wwwroot/", //服务器上这个文件要放在哪里
+	host: "", // 服务器的host地址 47.100.208.26
 	username: "root", // SSH用户名
-	password: "dream.233", // SSH密码
+	password: "", // SSH密码
 	port: 22, //SSH连接的端口
 };
